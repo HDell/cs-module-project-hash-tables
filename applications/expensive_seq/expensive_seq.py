dde8d5735c83801e254769bb074c3f8c2066df3f
Handell Desulme <tue73811@temple.edu>
2020-08-05 21:03:23 -0400

    completes 3/8 exercises

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,16 @@
 # Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
-
+    if (x, y, z) in cache.keys():
+        return cache[(x,y,z)]
+    elif x <= 0:
+        cache[(x,y,z)] = y + z
+        return cache[(x,y,z)]
+    else: # x > 0
+        cache[(x,y,z)] = expensive_seq(x-3, y+3, z*3) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-1, y+1, z)
+        return cache[(x,y,z)]
 
 if __name__ == "__main__":
     for i in range(10):
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,27 @@
 def word_count(s):
     # Your code here
-
+    dictionary = {}
+    ignored_characters = ''.join(c for c in s if (not c.isalnum()) and c!="'")
+    # replace = '":;,.-+=/\\|[]{}()*^&'
+    if len(ignored_characters) > 0:
+        # print(''.join(c.lower() for c in s if c.isalnum() or c=="'" or c==" "))
+        # print(''.join(dictionary[c.lower()] for c in s if c.isalnum() or c=="'" or c==" "))
+        
+        # new_string = s
+        # for c in replace:
+        #     new_string = new_string.replace(c, "")
+        
+        new_string = ''.join(c.lower() for c in s if c.isalnum() or c=="'" or c==" " or c == "\t" or c=="\n" or c=="\r")
+        # print(new_string)
+        new_list = new_string.replace("\t", " ").replace("\n", " ").replace("\r", " ").split(" ")
+        # print(new_list)
+        for word in new_list:
+            if len(word) > 0:
+                if word in dictionary.keys():
+                    dictionary[word] = dictionary[word] + 1
+                else:
+                    dictionary[word] = 1
+    return dictionary
 
 
 if __name__ == "__main__":
